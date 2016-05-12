9b6858e883a385757dddfa2c0a5b70995489013d
nehaparmarcs <np6.023@gmail.com>
2016-05-11 21:59:41 -0700

    Final Commit

--- src/main/java/com/service/FacebookServiceAdapter.java
+++ src/main/java/com/service/FacebookServiceAdapter.java
@@ -1,5 +1,6 @@
 package com.service;
 
+import java.io.IOException;
 import java.net.URLDecoder;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -11,11 +12,15 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 
+import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -27,7 +32,12 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 
-
+/**
+ * 
+ * db.fbeventdata.find({$or:[{"place_name" : "Miami Beach Club"},{"id" : "436934323166014"}]})
+ * @author neha
+ *
+ */
 
 
 @Path("/store")
@@ -39,11 +49,11 @@ public class FacebookServiceAdapter {
 	public Response pushData() {
 		FaceBookVO rdetails = new FaceBookVO();
 		try {	
-			String token= "EAACEdEose0cBAMSLSPaqdbX3GLgBpGmb7iRU01QHtoZBwZBP8jACOrSqluPDm7du2rmn3iUB0VAaJ2raFUDdnZAtsZB9hIEpwOHlZC9lJLb8gsAOmJOLeQkL6bNKQttDwrWacNjzRiGbzTbnOMDzgkARh9SwqVBFVMlbi13ZC3nwZDZD";
+			String token= "EAACEdEose0cBAEvKkGi94BZBT2yE2MjCggAVI9nmwLqHhJEBt4udQyZCTa1MiuAzWgqXFs5qE63GZAVlaHcZAWRGZCYM3uMnAVrU2bhO0g8JVeSsRRZBUzH4wwTbGy0nCjkZBcU1VCmZBBjZAkZBaz37XLsavax6OIS3PfI41wZBNe1ZAwZDZD";
 			
-			String[] allUrl = {"https://graph.facebook.com/v2.6/search?access_token="+token+"&pretty=1&q=san+jose&type=event&limit=25&after=NDkZD",
-					"https://graph.facebook.com/v2.6/search?q=san%20jose&type=event&access_token="+token,
-					"https://graph.facebook.com/v2.6/search?access_token="+token+"&pretty=1&q=san+jose&type=event&limit=25&after=MjQZD"
+			String[] allUrl = {//"https://graph.facebook.com/v2.6/search?access_token="+token+"&pretty=1&q=san+jose&type=event&limit=25&after=NDkZD",
+					"https://graph.facebook.com/v2.6/search?q=texas&type=event&access_token="+token
+					//,"https://graph.facebook.com/v2.6/search?access_token="+token+"&pretty=1&q=san+jose&type=event&limit=25&after=MjQZD"
 			};
 			for(String url :allUrl){
 				Client client = Client.create();
@@ -67,7 +77,7 @@ public class FacebookServiceAdapter {
 				insertIntoMongo(rdetails);
 			}
 			
-			searchData("World");
+			//searchData("World");
 			
 			System.out.println(rdetails);
 
@@ -89,7 +99,7 @@ public class FacebookServiceAdapter {
 				try {
 						for(Data data : fbVO.getData()){
 			
-						mongoClient = new MongoClient("54.165.58.89", 27017);
+						mongoClient = new MongoClient("54.186.78.152", 27017);
 					
 						@SuppressWarnings("deprecation")
 						DB db = mongoClient.getDB("ibm");
@@ -121,7 +131,6 @@ public class FacebookServiceAdapter {
 									dataDetails.put("place_location_zip", data.getPlace().getLocation().getZip());
 							//placeDetails.put("location", locDetails);
 							}
-								
 							
 							//dataDetails.put("place", placeDetails);
 							}
@@ -140,34 +149,46 @@ public class FacebookServiceAdapter {
 	@Path("/search/{searchText}")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response searchData(@PathParam("searchText") String searchText){
+	public String searchData(@PathParam("searchText") String searchText, 
+			@QueryParam("callback") String callback){
 		
 		MongoClient mongoClient;
 		FaceBookVO fbResObject = new FaceBookVO();
 		List<Data> dataList = new ArrayList<Data>();
-		String response = "";
+		//String response = "";
 		try {
-				
-				mongoClient = new MongoClient("54.165.58.89", 27017);
+				String inputText = URLDecoder.decode(searchText.trim(),"UTF-8");
+				mongoClient = new MongoClient("54.186.78.152", 27017);
 			
-				@SuppressWarnings("deprecation")
 				DB db = mongoClient.getDB("ibm");
 				DBCollection table = db.getCollection("fbeventdata");
 				
 				BasicDBObject regexQuery = new BasicDBObject();
 				//Pattern searchTxt = Pattern.compile(searchText, Pattern.CASE_INSENSITIVE);
-				  regexQuery.put("description",
-					new BasicDBObject("$regex", URLDecoder.decode(searchText,"UTF-8")));
+				  regexQuery.put("description",new BasicDBObject("$regex", inputText));
+				  
+				  
+				  //create regex to with or params
+				  DBObject clause1 = new BasicDBObject("description", new BasicDBObject("$regex", inputText).append("$options","i"));  
+				  DBObject clause2 = new BasicDBObject("name", new BasicDBObject("$regex", inputText).append("$options","i"));
+				  DBObject clause3 = new BasicDBObject("place_location_city", new BasicDBObject("$regex", inputText).append("$options","i"));
+				  BasicDBList or = new BasicDBList();
+				  or.add(clause1);
+				  or.add(clause2);
+				  or.add(clause3);
+				  
+				  DBObject query = new BasicDBObject("$or", or);
+				  System.out.println(query.toString());
+				  
+				  
 			                    //.append("$options", "m"));
 
-				  System.out.println(regexQuery.toString());
-				  ObjectMapper mapper = new ObjectMapper();
-					
+				  //System.out.println(regexQuery.toString());
+			
 				  
-				  DBCursor cursor8 = table.find(regexQuery);
+				  DBCursor cursor8 = table.find(query);
 				  while (cursor8.hasNext()) {
 					  DBObject nextDocument = cursor8.next();
-					  
 					  
 					  Data tempData = new Data();
 					  tempData.setDescription((String)nextDocument.get("description"));
@@ -222,10 +243,30 @@ public class FacebookServiceAdapter {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}		
-		return Response.status(201).entity(fbResObject).build();
+		
+		
+		  ObjectMapper mapper = new ObjectMapper();
+		  String jsonInString = "";
+		  try {
+		jsonInString = mapper.writeValueAsString(fbResObject);
+		} catch (JsonGenerationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(callback);
+		String response = "callbackMethod"+"("+jsonInString+");";
+		
+		return response;
 		
 	}
 	
+
 
 	public static void findFromMongo(){
 
@@ -233,7 +274,7 @@ public class FacebookServiceAdapter {
 			MongoClient mongoClient;
 			try {
 			
-				mongoClient = new MongoClient("54.165.58.89", 27017);
+				mongoClient = new MongoClient("54.186.78.152", 27017);
 				
 				@SuppressWarnings("deprecation")
 				DB db = mongoClient.getDB("ibm");
